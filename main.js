c71ba31d1cd87652610b5f9e528b3b6e314212ea
wakatsuki <waka@wakatsukiryoya.local>
2021-11-02 22:52:28 +0900

    ご指摘いただいた四則演算子が連続で入力できてしまう件、修正しました。ご確認よろしくお願い致します。

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 var obj = document.getElementById("result");
 
 function edit(ch) {
-  if (ch == "+" || ch == "-" || ch == "*" || ch == "/" || ch == ".") {
-    if (obj.value.slice(-1) == ch) return;
-    obj.value += ch;
-  } else if (ch == "00") {
+  if (ch == "00") {
     if (obj.value == "0" || obj.value == "") {
       obj.value = "0";
     } else {
       obj.value += ch;
     }
+  } else if (ch == ".") {
+    if (obj.value.slice(-1) == ch) return;
+    obj.value += ch;
   } else if (ch == "AC") {
     obj.value = "0";
   } else if (ch == "=") {
     obj.value = eval(obj.value);
   } else {
-    if (obj.value == "0") {
+    if (obj.value.slice(-1) == "+" || obj.value.slice(-1) == "-" || obj.value.slice(-1) == "*" || obj.value.slice(-1) == "/" || obj.value.slice(-1) == ".") {
+      if (ch == "+" || ch == "-" || ch == "*" || ch == "/" || ch == ".") {
+        obj.value = obj.value.slice(0,-1) + ch;
+      } else {
+        obj.value += ch;
+      }
+    } else if (obj.value == "0") {
       obj.value = ch;
     } else {
       obj.value += ch;
